8f93ce34b28064f72f3ede7b7da65de67a705bba
Álvaro Castellano Vela <alvaro.castellano.vela@gmail.com>
2024-06-08 19:05:32 +0000

    Fix lint

--- rabbitmq/rabbitmq.go
+++ rabbitmq/rabbitmq.go
@@ -7,37 +7,39 @@ import (
 	"strconv"
 )
 
-// RabbitmqConfig defines required data for connecting to RabbitMQ server
+// Config defines required data for connecting to RabbitMQ server
 
-type config struct {
-	Host     string
-	Port     int
-	User     string
-	Password string
+type Config struct {
+	host     string
+	port     int
+	user     string
+	password string
 }
 
-func NewRabbitmqConfig() (*config, error) {
-	config := new(config)
+// NewConfig validates and returns Config instance
+
+func NewConfig() (*Config, error) {
+	config := new(Config)
 
 	// Get Host from RABBITMQ_HOST env variable
-	config.Host = cmp.Or(os.Getenv("RABBITMQ_HOST"), "localhost")
+	config.host = cmp.Or(os.Getenv("RABBITMQ_HOST"), "localhost")
 
 	// Get Port from RABBITMQ_PORT env variable and validate its value
 	var portAtoiErr error
-	config.Port, portAtoiErr = strconv.Atoi(cmp.Or(os.Getenv("RABBITMQ_PORT"), "5672"))
+	config.port, portAtoiErr = strconv.Atoi(cmp.Or(os.Getenv("RABBITMQ_PORT"), "5672"))
 	if portAtoiErr != nil {
 		return config, portAtoiErr
 	} else {
-		if config.Port <= 0 || config.Port >= 65536 {
+		if config.port <= 0 || config.port >= 65536 {
 			return config, errors.New("RabbitMQ portvalue must be between 1 and 65535")
 		}
 	}
 
 	// Get User from RABBITMQ_USER env variable
-	config.User = cmp.Or(os.Getenv("RABBITMQ_USER"), "guest")
+	config.user = cmp.Or(os.Getenv("RABBITMQ_USER"), "guest")
 
 	// Get Password from RABBITMQ_PASSWORD env variable
-	config.Password = cmp.Or(os.Getenv("RABBITMQ_PASSWORD"), "guest")
+	config.password = cmp.Or(os.Getenv("RABBITMQ_PASSWORD"), "guest")
 
 	return config, nil
 }
--- rabbitmq/rabbitmq_test.go
+++ rabbitmq/rabbitmq_test.go
@@ -9,22 +9,22 @@ import (
 
 func TestRabbitmqConfigWithoutEnvVariables(t *testing.T) {
 
-	config, err := NewRabbitmqConfig()
+	config, err := NewConfig()
 
 	if err != nil {
-		t.Errorf("NewRabbitmqConfig method without any env varible suited shouldn't fail.")
+		t.Errorf("NewConfig method without any env varible suited shouldn't fail.")
 	} else {
-		if config.Host != "localhost" {
-			t.Errorf("RabbitmqConfig Host should be \"localhost\" but was \"%s\".", config.Host)
+		if config.host != "localhost" {
+			t.Errorf("Rabbitmq config.host should be \"localhost\" but was \"%s\".", config.host)
 		}
-		if config.Port != 5672 {
-			t.Errorf("RabbitmqConfig Host should be 5672 but was %d.", config.Port)
+		if config.port != 5672 {
+			t.Errorf("Rabbitmq config.host should be 5672 but was %d.", config.port)
 		}
-		if config.User != "guest" {
-			t.Errorf("RabbitmqConfig User should be \"guest\" but was \"%s\".", config.User)
+		if config.user != "guest" {
+			t.Errorf("Rabbitmq config.user should be \"guest\" but was \"%s\".", config.user)
 		}
-		if config.Password != "guest" {
-			t.Errorf("RabbitmqConfig Password should be \"guest\" but was \"%s\".", config.Password)
+		if config.password != "guest" {
+			t.Errorf("Rabbitmq config.password should be \"guest\" but was \"%s\".", config.password)
 		}
 	}
 }
@@ -33,10 +33,10 @@ func TestRabbitmqConfigWithStringAsPortValue(t *testing.T) {
 
 	os.Setenv("RABBITMQ_PORT", "invalidport")
 
-	_, err := NewRabbitmqConfig()
+	_, err := NewConfig()
 
 	if err == nil {
-		t.Errorf("NewRabbitmqConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
+		t.Errorf("NewConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
 	}
 }
 
@@ -44,10 +44,10 @@ func TestRabbitmqConfigWithInvalidPortValue1(t *testing.T) {
 
 	os.Setenv("RABBITMQ_PORT", "65536")
 
-	_, err := NewRabbitmqConfig()
+	_, err := NewConfig()
 
 	if err == nil {
-		t.Errorf("NewRabbitmqConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
+		t.Errorf("NewConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
 	}
 }
 
@@ -55,10 +55,10 @@ func TestRabbitmqConfigWithInvalidPortValue2(t *testing.T) {
 
 	os.Setenv("RABBITMQ_PORT", "0")
 
-	_, err := NewRabbitmqConfig()
+	_, err := NewConfig()
 
 	if err == nil {
-		t.Errorf("NewRabbitmqConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
+		t.Errorf("NewConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
 	}
 }
 
@@ -66,10 +66,10 @@ func TestRabbitmqConfigWithInvalidPortValue3(t *testing.T) {
 
 	os.Setenv("RABBITMQ_PORT", "-1")
 
-	_, err := NewRabbitmqConfig()
+	_, err := NewConfig()
 
 	if err == nil {
-		t.Errorf("NewRabbitmqConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
+		t.Errorf("NewConfig method with \"RABBITMQ_PORT\" env variable containing invalid value should fail.")
 	}
 }
 
@@ -80,22 +80,22 @@ func TestRabbitmqConfigWithEnvVariables(t *testing.T) {
 	os.Setenv("RABBITMQ_USER", "user")
 	os.Setenv("RABBITMQ_PASSWORD", "password")
 
-	config, err := NewRabbitmqConfig()
+	config, err := NewConfig()
 
 	if err != nil {
-		t.Errorf("NewRabbitmqConfig method with valid env varibles suited shouldn't fail.")
+		t.Errorf("NewConfig method with valid env varibles suited shouldn't fail.")
 	} else {
-		if config.Host != "127.0.0.1" {
-			t.Errorf("RabbitmqConfig Host should be \"127.0.0.1\" but was \"%s\".", config.Host)
+		if config.host != "127.0.0.1" {
+			t.Errorf("Rabbitmq config.host should be \"127.0.0.1\" but was \"%s\".", config.host)
 		}
-		if config.Port != 1123 {
-			t.Errorf("RabbitmqConfig Host should be 1123 but was %d.", config.Port)
+		if config.port != 1123 {
+			t.Errorf("Rabbitmq config.host should be 1123 but was %d.", config.port)
 		}
-		if config.User != "user" {
-			t.Errorf("RabbitmqConfig User should be \"user\" but was \"%s\".", config.User)
+		if config.user != "user" {
+			t.Errorf("Rabbitmq config.user should be \"user\" but was \"%s\".", config.user)
 		}
-		if config.Password != "password" {
-			t.Errorf("RabbitmqConfig Password should be \"password\" but was \"%s\".", config.Password)
+		if config.password != "password" {
+			t.Errorf("Rabbitmq config.password should be \"password\" but was \"%s\".", config.password)
 		}
 	}
 }
